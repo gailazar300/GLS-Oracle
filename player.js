59ce2ea1bb6644ca9c6cf1279bd50b4925454a54
Administrator <gailazar300@gmail.com>
2020-10-08 18:58:05 +0300

    show all steps on screen

--- player.js
+++ player.js
@@ -8,11 +8,24 @@ var script2 = document.createElement('script');
 script2.src = 'https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867&callback=processData';
 //script2.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script2);
+
+
+
 function processData(myObj) {
     console.log(myObj);
-    let obj =createElementFromHTML(myObj.data.tiplates.tip);
-    document.body.prepend(obj);
-    $('.popover-content [data-iridize-id="content"]').html(myObj.data.structure.steps[0].action.contents["#content"]);
+    $('<link rel="stylesheet" href="https://guidedlearning.oracle.com/player/latest/static/css/stTip.css" type="text/css" />').appendTo('head');
+    $('<style type="text/css"> ' + myObj.data.css + '</style>').appendTo('head');
+    // let obj =createElementFromHTML('<div  class="sttip">   	<div class="tooltip in"> <div class="tooltip-arrow"></div><div class="tooltip-arrow second-arrow"></div><div class="popover-inner"><div class=el'+1+'> '+myObj.data.tiplates.tip+'</div></div></div></div>');
+    //document.getElementById('hplogo').after(obj);
+    // $('.popover-content [data-iridize-id="content"]').html(myObj.data.structure.steps[0].action.contents["#content"]);
+
+
+    for (i = 0; i < 5; i++) {
+        let obj =createElementFromHTML('<div  class="sttip">   	<div class="tooltip in"> <div class="tooltip-arrow"></div><div class="tooltip-arrow second-arrow"></div><div class="popover-inner"><div class=el'+i+'> '+myObj.data.tiplates.tip+'</div></div></div></div>');
+        $(myObj.data.structure.steps[i].action.selector).after(obj);
+        $('.el'+i+' .popover-content [data-iridize-id="content"]').html(myObj.data.structure.steps[i].action.contents["#content"]);
+
+    }
 
 }
 
@@ -24,3 +37,4 @@ function createElementFromHTML(htmlString) {
     // Change this to div.childNodes to support multiple top-level nodes
     return div.firstChild;
 }
+
