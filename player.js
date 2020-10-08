50b5ad8abbb65158cb76cf3656e037a056aa748c
Administrator <gailazar300@gmail.com>
2020-10-08 23:48:36 +0300

    orginize the code

--- player.js
+++ player.js
@@ -1,36 +1,45 @@
 
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.1.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
+injectJquery();
 
 var script2 = document.createElement('script');
 script2.src = 'https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867&callback=processData';
-//script2.type = 'text/javascript';
+script2.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script2);
 
-
+function injectJquery() {
+    var script = document.createElement('script');
+    script.src = 'https://code.jquery.com/jquery-3.1.1.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
 
 function processData(myObj) {
-    console.log(myObj);
-    $('<link rel="stylesheet" href="https://guidedlearning.oracle.com/player/latest/static/css/stTip.css" type="text/css" />').appendTo('head');
-    $('<style type="text/css"> ' + myObj.data.css + '</style>').appendTo('head');
+    injectCss(myObj.data.css);
+    let numOfSteps = myObj.data.structure.steps.length;
+    for (i = 0; i < numOfSteps; i++) {
+        let stepType = myObj.data.tiplates[myObj.data.structure.steps[i].action.type];
 
-    for (i = 0; i < 5; i++) {
-        let obj =createElementFromHTML('<div  class="sttip">   	<div class="tooltip in"> <div class="tooltip-arrow"></div><div class="tooltip-arrow second-arrow"></div><div class="popover-inner"><div class=el'+i+'> '+myObj.data.tiplates.tip+'</div></div></div></div>');
+        let obj = createTipElementFromHTML(stepType ,i);
         $(myObj.data.structure.steps[i].action.selector).after(obj);
-        $('.el'+i+' .popover-content [data-iridize-id="content"]').html(myObj.data.structure.steps[i].action.contents["#content"]);
+
+        $('.stepNumber'+i+' .steps-count [data-iridize-role="stepCount"]').html("<span>"+(i+1)+"</span>");
+        $('.stepNumber'+i+' .stepNumber'+i+' span[data-iridize-role="stepsCount"]').html('<span>'+numOfSteps+'</span>');
+        $('.stepNumber'+i+' .popover-content [data-iridize-id="content"]').html(myObj.data.structure.steps[i].action.contents["#content"]);
+
         $("[class='tooltip in']").removeClass('tooltip in').addClass("tooltip in "+myObj.data.structure.steps[i].action.placement+" "+myObj.data.structure.steps[i].action.classes);
     }
 
 }
 
-
-function createElementFromHTML(htmlString) {
+function injectCss(JsonsCss){
+    $('<link rel="stylesheet" href="https://guidedlearning.oracle.com/player/latest/static/css/stTip.css" type="text/css" />').appendTo('head');
+    $('<style type="text/css"> ' + JsonsCss + '</style>').appendTo('head');
+}
+function createTipElementFromHTML(htmlString, stepIndex) {
+    let wrapperPrefix ='<div  class="sttip">   	<div class="tooltip in"> <div class="tooltip-arrow"></div><div class="tooltip-arrow second-arrow"></div><div class="popover-inner">';
+    let wrapperSuffix ='</div></div></div>';
     var div = document.createElement('div');
-    div.innerHTML = htmlString.trim();
-
-    // Change this to div.childNodes to support multiple top-level nodes
+    div.innerHTML ='<div class=stepNumber'+stepIndex+'> '+ wrapperPrefix+ htmlString + wrapperSuffix +' </div>';
     return div.firstChild;
 }
 
